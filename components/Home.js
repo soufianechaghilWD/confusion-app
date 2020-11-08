3da9c15002f56c68876bc4041a9926f0fed4e60c
soufiane chaghil <soufianechaghil1998@gmail.com>
2020-11-08 20:42:46 +0000

    React Native Navigation Part 3

--- components/Home.js
+++ components/Home.js
@@ -1,11 +1,40 @@
 import React, { useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, ScrollView, Text } from 'react-native';
+import { Card } from 'react-native-elements';
+import { DISHES } from '../shared/dishes';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
+
 
 function Home() {
+
+    function RenderItem ({item}) {
+        if(item!= null){
+           return(
+           <Card>
+               <Card.Title>{item.name}</Card.Title>
+               <Card.FeaturedSubtitle>{item.designation}</Card.FeaturedSubtitle>
+               <Card.Image source={require('./images/uthappizza.png')} />
+               <Text style={{margin: 10}}>
+                   {item.description}
+               </Text>
+           </Card>)
+        }else{
+            return(<View></View>)
+        }
+    }
+
+    const [dishes, setDishes] = useState(DISHES)
+    const [leaders, setLeaders] = useState(LEADERS)
+    const [promotions, setPromotions] = useState(PROMOTIONS)
+
+
     return (
-        <View>
-            <Text>Home Component</Text>
-        </View>
+        <ScrollView>
+            <RenderItem item = {dishes.filter((dish) => dish.featured)[0]}/>
+            <RenderItem item = {promotions.filter((promo) => promo.featured)[0]}/>
+            <RenderItem item = {leaders.filter((leader) => leader.featured)[0]}/>
+        </ScrollView>
     )
 }
 
