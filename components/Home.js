e02715c9c8f3d5814efec6e117985965eee7d5e2
soufiane chaghil <soufianechaghil1998@gmail.com>
2020-11-16 20:43:06 +0000

    Animations

--- components/Home.js
+++ components/Home.js
@@ -1,5 +1,5 @@
-import React, {useEffect} from 'react';
-import { View, ScrollView, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Animated, Easing } from 'react-native';
 import { Card } from 'react-native-elements';
 import { useStateValue } from "./stateProvider";
 import { baseUrl } from '../shared/baseUrl';
@@ -8,7 +8,37 @@ import Loading from './Loading'
 function Home(props) {
 
     const [{dishes, promos, leaders}, dispatch] = useStateValue();
+    const [animatedValue, setAnimatedValue] = useState(new Animated.Value(0));
+    
 
+    const animate = () => {
+        animatedValue.setValue(0)
+        Animated.timing(
+            animatedValue,
+            {
+                toValue: 8,
+                duration: 8000,
+                easing: Easing.linear
+            }
+        ).start(() => animate())
+    }
+
+    useEffect(() => {
+        animate()
+    }, [])
+
+    const xpos1 = animatedValue.interpolate({
+        inputRange: [0, 1, 3, 5, 8],
+        outputRange: [1200, 600, 0, -600, -1200]
+    })
+    const xpos2 = animatedValue.interpolate({
+        inputRange: [0, 2, 4, 6, 8],
+        outputRange: [1200, 600, 0, -600, -1200]
+    })
+    const xpos3 = animatedValue.interpolate({
+        inputRange: [0, 3, 5, 7, 8],
+        outputRange: [1200, 600, 0, -600, -1200 ]
+    })
 
     function RenderItem ({item}) {
         if(item!= null){
@@ -29,16 +59,25 @@ function Home(props) {
     
 
     return (
-        <ScrollView>
-            {dishes.length === 0 ? <Loading /> : 
-            <RenderItem item = {dishes?.filter((dish) => dish.featured)[0]}/>}
-            {promos.length === 0 ? <Loading /> :
-            <RenderItem item = {promos?.filter((promo) => promo.featured)[0]}/>            
-            }
-            {leaders.length === 0 ? <Loading /> :
-            <RenderItem item = {leaders?.filter((leader) => leader.featured)[0]}/>
-            }
-        </ScrollView>
+        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
+
+            <Animated.View style={{ width: '100%', transform: [{translateX: xpos1}]}}>
+                {dishes.length === 0 ? <Loading /> : 
+                <RenderItem item = {dishes?.filter((dish) => dish.featured)[0]}/>}
+            </Animated.View>
+            <Animated.View style={{ width: '100%',  transform: [{translateX: xpos2}]}}>
+                {promos.length === 0 ? <Loading /> :
+                <RenderItem item = {promos?.filter((promo) => promo.featured)[0]}/>            
+                }
+            </Animated.View>
+            <Animated.View style={{ width: '100%',  transform: [{translateX: xpos3}]}}>
+                {leaders.length === 0 ? <Loading /> :
+                <RenderItem item = {leaders?.filter((leader) => leader.featured)[0]}/>
+                }
+            </Animated.View>
+
+        </View>
+
     )
 }
 
